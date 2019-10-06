5f1b70e16e04569a0cfd49f6db1103fc0421eea5
chowdapowa9 <tristanthibado@yahoo.com>
2019-10-06 16:05:48 -0700

    Fixed Ready event and made it update hourly

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,7 @@
 module.exports = (client) => {
   // Setting activity
-  client.user.setActivity(`ACNH with ${client.users.size} users!`);
+  const guild = client.guilds.get('guildID');
+  setInterval(() => client.user.setActivity(`ACNH with ${guild.memberCount} users!`), 3600000);
 
   // Logging a ready message on first boot
   console.log(`Ready to follow orders sir, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
