36faf071c58fec1b0b782e4cce2280c21177fd89
chowdapowa9 <tristanthibado@yahoo.com>
2019-10-06 22:03:47 -0700

    Fixed the ready event to log correct user count

--- events/ready.js
+++ events/ready.js
@@ -4,5 +4,5 @@ module.exports = (client) => {
   setInterval(() => client.user.setActivity(`ACNH with ${guild.memberCount} users!`), 30000);
 
   // Logging a ready message on first boot
-  console.log(`Ready to follow orders sir, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+  console.log(`Ready to follow orders sir, with ${guild.memberCount} users, in ${guild.channels.size} channels of ${client.guilds.size} guilds.`);
 };
