28eca2984f131196aa07876ac4d2d4c14bfe12b0
chowdapowa9 <tristanthibado@yahoo.com>
2019-10-06 17:59:21 -0700

    Activity update from 1 hour to 30 seconds

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 module.exports = (client) => {
   // Setting activity
   const guild = client.guilds.first();
-  setInterval(() => client.user.setActivity(`ACNH with ${guild.memberCount} users!`), 3600000);
+  setInterval(() => client.user.setActivity(`ACNH with ${guild.memberCount} users!`), 30000);
 
   // Logging a ready message on first boot
   console.log(`Ready to follow orders sir, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
