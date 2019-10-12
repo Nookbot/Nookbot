5905215ade2bf72357c9ac7281df1715ada639c4
chowdapowa9 <tristanthibado@yahoo.com>
2019-10-12 00:34:43 -0700

    Fixing the Message Update Event

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,16 +1,24 @@
 const Discord = require('discord.js');
 
 module.exports = async (client, oldMessage, newMessage) => {
-  // Field value length limit for embeds is 1024
+  // Ignore all bots
+  if (newMessage.author.bot) {
+    return;
+  }
+
+  // Description value length limit for embeds is 1024
   const oldMsg = oldMessage.content.length > 499 ? `${oldMessage.content.slice(0,496)}...` : oldMessage.content.slice(0,499);
   const newMsg = newMessage.content.length > 499 ? `${newMessage.content.slice(0,496)}...` : newMessage.content.slice(0,499);
 
   const embed = new Discord.RichEmbed()
+    .setTitle('Jump to Message')
+    .setURL(`https://discordapp.com/channels/${newMessage.guild.id}/${newMessage.channel.id}/${newMessage.id}`)
     .setAuthor(newMessage.author.tag, newMessage.author.displayAvatarURL)
     .setColor('#00e5ff')
     .setTimestamp()
     .setFooter(`ID: ${newMessage.author.id}`)
-    .addField(`**Message Edited in** <#${newMessage.channel.id}> [Jump](https://discordapp.com/channels/${newMessage.guild.id}/${newMessage.channel.id}/${newMessage.id} 'Jump to Message')`, `**Before:** ${oldMsg}\n**+After:** ${newMsg}`);
+    .addDescription(`**Message edited in** <#${newMessage.channel.id}>`)
+    .addField('\u200b', `**Before:** ${oldMsg}\n**+After:** ${newMsg}`);
 
   newMessage.guild.channels.get(client.getSettings(newMessage.guild).actionLog).send(embed);
 };
