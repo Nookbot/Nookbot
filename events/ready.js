f9ea02530061156254bfe79905414e449b281c1e
Phoenix1128 <pdinsdale1128@gmail.com>
2019-10-14 08:40:09 -0400

    Countdown msg

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,38 @@
+const moment = require('moment');
+
 module.exports = (client) => {
   // Setting activity
   const guild = client.guilds.first();
   setInterval(() => client.user.setActivity(`ACNH with ${guild.memberCount} users!`), 30000);
+
+  const now = moment();
+  const releaseDate = moment([2020, 2, 20]);
+
+  if (now < releaseDate) {
+    const postTime = moment().set({ h: '20', m: '00', ms: '00' });
+    let timeUntilFirstPost;
+
+    if (now > postTime) {
+      timeUntilFirstPost = now - postTime;
+    } else {
+      timeUntilFirstPost = postTime - now;
+    }
+
+    const post = () => {
+      const daysUntilRelease = releaseDate.diff(moment(), 'days');
+
+      const channel = guild.channels.get('494376688877174785');
+      return channel.send(`**Animal Crossing: New Horizons** releases in **$${daysUntilRelease} days!**`);
+    };
+
+    setTimeout(() => {
+      post();
+
+      setInterval(() => {
+        post();
+      }, 3600000 * 24);
+    }, timeUntilFirstPost);
+  }
 
   // Logging a ready message on first boot
   console.log(`Ready to follow orders sir, with ${guild.memberCount} users, in ${guild.channels.size} channels of ${client.guilds.size} guilds.`);
