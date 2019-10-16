3031c04c73151b9231216450c0c0ec96da6e7856
chowdapowa9 <tristanthibado@yahoo.com>
2019-10-15 17:44:15 -0700

    File size decimals.

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -24,7 +24,7 @@ module.exports = async (client, message) => {
     let attachList = '';
 
     message.attachments.forEach((value) => {
-      const fileSize = value.filesize > 1048576 ? `${Math.floor(value.filesize / 1048576)} MB` : `${Math.floor(value.filesize / 1024)} KB`;
+      const fileSize = value.filesize > 1048576 ? `${(value.filesize / 1048576).toFixed(2)} MB` : `${(value.filesize / 1024).toFixed(2)} KB`;
       attachList += `\n${value.filename} | ${fileSize}`;
     });
 
