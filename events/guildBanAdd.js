a29b694d3feb926665dc8719bde5de0563f16ad3
chowdapowa9 <tristanthibado@yahoo.com>
2019-11-05 17:16:01 -0800

    Guild Ban/Unban events

---
+++ events/guildBanAdd.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+
+module.exports = async (client, guild, user) => {
+  const embed = new Discord.RichEmbed()
+    .setAuthor(user.tag, user.displayAvatarURL)
+    .setColor('#ff9292')
+    .setTimestamp()
+    .setFooter(`ID: ${user.id}`)
+    .setThumbnail(user.displayAvatarURL)
+    .setTitle('**Member Banned**')
+    .setDescription(user.id);
+
+  guild.channels.get(client.getSettings(guild).modLog).send(embed);
+};
