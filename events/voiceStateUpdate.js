35013f972c303a8c78b591979a6f8295d7df8968
chowdapowa9 <tristanthibado@yahoo.com>
2019-12-11 02:49:11 -0800

    Fixing some tiny things

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -3,8 +3,8 @@ module.exports = async (client, oldMember, newMember) => {
   if (client.voiceConnections.get(oldMember.guild.id)
       && oldMember.guild.channels.get(client.getSettings(oldMember.guild).voice).members.size === 1) {
     client.songQueue.playing = false;
-    client.songQueue.voiceChannel.leave();
-    client.songQueue.connection.disconnect();
+    if (client.songQueue.voiceChannel) client.songQueue.voiceChannel.leave();
+    if (client.songQueue.connection) client.songQueue.connection.disconnect();
     client.songQueue.connection = null;
     client.songQueue.voiceChannel = null;
     return oldMember.guild.channels.get(client.getSettings(oldMember.guild).voiceText).send('Everyone left voice chat, so the music stopped.');
