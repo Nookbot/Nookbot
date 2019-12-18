4e7474eccdca30207b28b74f82c03fed14e5ab31
chowdapowa9 <tristanthibado@yahoo.com>
2019-12-18 04:42:06 -0800

    Debug userUpdate

--- events/userUpdate.js
+++ events/userUpdate.js
@@ -1,13 +1,14 @@
 const Discord = require('discord.js');
 
 module.exports = async (client, oldUser, newUser) => {
-  if (oldUser.tag !== newUser.tag) {
+  console.log(`userUpdate Event:\noldUser: ${oldUser.tag} | ${oldUser.username} ${oldUser.discriminator}\nnewUser: ${newUser.tag} | ${newUser.username} ${newUser.discriminator}`)
+  if (oldUser.username !== newUser.username || oldUser.discriminator !== newUser.discriminator) {
     const embed = new Discord.RichEmbed()
       .setAuthor(newUser.tag, newUser.displayAvatarURL)
       .setTimestamp()
       .setColor('#00e5ff')
       .setFooter(`ID: ${newUser.id}`)
-      .addField('**Username Update**', `**Before:**${oldUser.tag}\n**+After:**${newUser.tag}`);
+      .addField('**Username Update**', `**Before:**${oldUser.username}#${oldUser.discriminator}\n**+After:**${newUser.username}#${newUser.discriminator}`);
 
     client.userDB.ensure(newUser.id, client.config.userDBDefaults);
     client.userDB.push(newUser.id, { timestamp: Date.now(), username: newUser.tag }, 'usernames');
