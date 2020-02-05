69d6a59b2035a10d393d005a33a069b4206d9689
chowdapowa9 <tristanthibado@yahoo.com>
2020-02-05 02:12:43 -0800

    Fixing ban command and update news cmd

--- commands/misc/news.js
+++ commands/misc/news.js
@@ -1,16 +1,19 @@
 module.exports.run = async (client, message, args) => {
-  const channel = message.guild.channels.get('588783352463622144');
+  const channel = message.mentions.channels.first();
   const attachments = message.attachments.map((a) => a.url);
   let role;
-  switch (args[0]) {
+  switch (args[1]) {
+    case 'gamenews':
     case 'game':
     case 'g':
       role = message.guild.roles.get('588955307817041946');
       break;
+    case 'servernews':
     case 'server':
     case 's':
       role = message.guild.roles.get('588955864677744642');
       break;
+    case 'eventnews':
     case 'event':
     case 'e':
       role = message.guild.roles.get('588955576562614273');
@@ -20,22 +23,21 @@ module.exports.run = async (client, message, args) => {
       return;
   }
   await role.edit({ mentionable: true });
-  await channel.send(`${args.slice(1).join(' ')}\n<@&${role.id}>`, { files: attachments });
+  await channel.send(`${args.slice(2).join(' ')}\n<@&${role.id}>`, { files: attachments });
   await role.edit({ mentionable: false });
-  return;
 };
 
 module.exports.conf = {
   guildOnly: true,
   aliases: ['n'],
   permLevel: 'Admin',
-  args: 1,
+  args: 2,
 };
 
 module.exports.help = {
   name: 'news',
   category: 'misc',
   description: 'Pings members that have the specified news role.',
-  usage: 'news <game|server|event> <message>',
-  details: '<game|server|event> => Which of the news roles to ping.\n<message> => The content of the message to send with the ping.',
+  usage: 'news <#channel> <game|server|event> <message>',
+  details: '<#channel> => The channel to post in.\n<game|server|event> => The news role to ping.\n<message> => The content of the message to send with the ping.',
 };
--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 module.exports.run = async (client, message, args, level) => {
   // Setting member to first member mentioned
-  let member = message.mentions.members.first();
+  let member = message.mentions.members.first() || parseInt(args[0], 10) ? args[0] : undefined;
 
   if (!member) {
     const searchedMember = client.searchMember(args[0]);
@@ -20,10 +20,10 @@ module.exports.run = async (client, message, args, level) => {
   const reason = args[1] ? args.slice(1).join(' ') : 'No reason provided';
 
   // Bans the member
-  return message.guild.ban(member, { reason }).then((memberBanned) => {
+  return message.guild.ban(member, reason).then((memberBanned) => {
     // If ban is successful, display this
     client.success(message.channel, 'Ban Successful!', `I've successfully banned **${memberBanned.user.tag}**!`);
-  }).catch((error) => client.error(message.channel, 'Ban Failed!', `I've failed to ban this member! Error: ${error}`));
+  }).catch((error) => client.error(message.channel, 'Ban Failed!', `I've failed to ban this member! ${error}`));
 };
 
 module.exports.conf = {
