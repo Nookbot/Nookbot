4a3f31b2f20c14638f92e2ac97fa1943f9ad9ea6
chowdapowa9 <tristanthibado@yahoo.com>
2020-02-13 01:22:47 -0800

    Rename infractions to infractionDB

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ client.twitter = new Twitter({
 // Start up the twitter webhook listener
 client.twitterHook = new Discord.WebhookClient(client.config.twitterHookID, client.config.twitterHookToken);
 
-Object.assign(client, Enmap.multi(['settings', 'enabledCmds', 'userDB', 'emojiDB', 'villagerDB', 'tags', 'playlist', 'infractions'], { ensureProps: true }));
+Object.assign(client, Enmap.multi(['settings', 'enabledCmds', 'userDB', 'emojiDB', 'villagerDB', 'tags', 'playlist', 'infractionDB'], { ensureProps: true }));
 
 client.login(config.token).catch(() => {
   const interval = setInterval(() => {
