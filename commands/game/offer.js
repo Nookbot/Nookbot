a3958676f9b8ca58ded7bb58b6bd62cb1d633ee1
chowdapowa9 <tristanthibado@yahoo.com>
2020-03-31 18:38:56 -0700

    Offer command round robin

--- commands/game/offer.js
+++ commands/game/offer.js
@@ -8,7 +8,10 @@ module.exports.run = async (client, message, args) => {
 
   const villager = findBest(args.join(' '), client.villagerDB.keyArray()).bestMatch;
   if (villager.rating > 0.1) {
-    const vilAdopters = client.villagerDB.get(villager.target).adopters.filter((m) => message.guild.members.cache.has(m));
+    const vilAdopters = client.villagerDB.get(villager.target, 'adopters').filter((m) => message.guild.members.cache.has(m));
+    const curOffset = client.villagerDB.get(villager.target, 'offset');
+    const pingOffset = Math.min(curOffset, Math.max(vilAdopters.length - 10, 0));
+
     // Clear the list of members that are no longer on the server
     client.villagerDB.set(villager.target, vilAdopters, 'adopters');
 
@@ -18,9 +21,14 @@ module.exports.run = async (client, message, args) => {
 
     const decision = await client.reactPrompt(message, `You are about to ping **${vilAdopters.length > 10 ? `10 (+${vilAdopters.length - 10} not pinged)` : `${vilAdopters.length}`}** members of this server that wish to adopt **${villager.target}**.\n\n**WARNING: IF YOU CHOOSE TO PING AND DO NOT GIVE SOMEONE THE VILLAGER, YOU WILL BE GIVEN TEN STINGS!**\n\n**ONLY USE THIS COMMAND IF THE VILLAGER YOU ARE OFFERING IS IN BOXES MOVING OUT!**\n\nDo you wish to ping **${vilAdopters.length > 10 ? `10 (+${vilAdopters.length - 10} not pinged)` : `${vilAdopters.length}`}** members?`);
     if (decision) {
+      if (curOffset !== 0 && curOffset + 10 >= vilAdopters.length) {
+        client.villagerDB.set(villager.target, 0, 'offset');
+      } else if (curOffset + 10 < vilAdopters.length) {
+        client.villagerDB.math(villager.target, '+', 10, 'offset');
+      }
       const msgArr = [];
-      vilAdopters.slice(0, 10).forEach((memID, i) => {
-        msgArr.push(`#${i + 1} - <@${memID}> - ${client.userDB.ensure(memID, client.config.userDBDefaults).friendcode || 'Ask'}`);
+      vilAdopters.slice(pingOffset, pingOffset + 10).forEach((memID, i) => {
+        msgArr.push(`#${pingOffset + i + 1} - <@${memID}> - ${client.userDB.ensure(memID, client.config.userDBDefaults).friendcode || 'Ask'}`);
       });
       const extra = vilAdopters.length > 10 ? vilAdopters.length - 10 : 0;
       const msg = `The following members are looking to adopt **${villager.target}**:\nPosition - Member - Friend Code\n${msgArr.join('\n')}${extra !== 0 ? `\nAnd **${extra}** other${extra === 1 ? '' : 's'} not pinged due to the limit of 10 pinged members.` : ''}\nYou are ultimately responsible for how to choose someone to adopt your villager, whether it be first to respond, first on the list, by random, or your pick.`;
