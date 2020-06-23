0c3381d119497e2125d61ee13cc4bde967fecc10
chowdapowa9 <tristanthibado@yahoo.com>
2020-06-22 22:39:44 -0700

    Remove fetchAll from large enmaps

--- index.js
+++ index.js
@@ -129,7 +129,8 @@ client.twitter = new Twitter({
 // Start up the twitter webhook listener
 client.twitterHook = new Discord.WebhookClient(client.config.twitterHookID, client.config.twitterHookToken);
 
-Object.assign(client, Enmap.multi(['enabledCmds', 'userDB', 'emojiDB', 'villagerDB', 'tags', 'playlist', 'infractionDB', 'sessionDB', 'muteDB', 'memberStats', 'reactionRoleDB'], { ensureProps: true }));
+Object.assign(client, Enmap.multi(['enabledCmds', 'emojiDB', 'villagerDB', 'tags', 'playlist',  'sessionDB', 'muteDB', 'reactionRoleDB'], { ensureProps: true }));
+Object.assign(client, Enmap.multi(['userDB', 'infractionDB', 'memberStats'], { fetchAll: false, ensureProps: true }));
 
 client.login(config.token).then(() => {
   console.log('Bot successfully logged in.');
