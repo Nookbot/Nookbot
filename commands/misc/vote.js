102ba3442d45b413f4644d2bf53ac676cd3848d7
Phoenix1128 <pdinsdale1128@gmail.com>
2020-07-18 15:35:59 -0400

    Add time message is posted in message edit and message delete embeds

--- commands/misc/vote.js
+++ commands/misc/vote.js
@@ -1,10 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 module.exports.run = async (client, message, args, level) => {
-  const msgToReact = await message.channel.messages.fetch(args[0]);
-
-  if (!msgToReact) {
-    return client.error(message.channel, 'No Message Found!', 'No message was found! Please use a valid message id from **THIS** channel!');
-  }
+  const msgToReact = await message.channel.messages.fetch(args[0])
+    .catch(() => client.error(message.channel, 'No Message Found!', 'No message was found! Please use a valid message id from **THIS** channel!'));
 
   await msgToReact.react(client.emoji.thumbsUp);
   await msgToReact.react(client.emoji.thumbsDown);
