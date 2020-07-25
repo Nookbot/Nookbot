893305f281d627b39527da279b68db197fcaec87
Phoenix1128 <pdinsdale1128@gmail.com>
2020-07-24 22:27:12 -0400

    Improve vote command

--- commands/misc/vote.js
+++ commands/misc/vote.js
@@ -1,24 +1,28 @@
 // eslint-disable-next-line no-unused-vars
 module.exports.run = async (client, message, args, level) => {
-  let fail = false;
-  const msgToReact = await message.channel.messages.fetch(args[0])
-    .catch(() => {
-      fail = true;
-      return client.error(message.channel, 'No Message Found!', 'No message was found! Please use a valid message id from **THIS** channel!');
-    });
+  let msgToReact = message.channel.messages.cache.get(args[0]);
 
-  if (fail) {
-    return;
+  // eslint-disable-next-line no-restricted-globals
+  if (!msgToReact) {
+    if (parseInt(args[0], 10)) {
+      try {
+        msgToReact = await message.channel.messages.fetch(args[0]);
+      } catch (err) {
+        return client.error(message.channel, 'No Message Found!', 'No message was found! Please use a valid message id from **THIS** channel!');
+      }
+    } else {
+      return client.error(message.channel, 'Not a Message ID!', 'Please use this command with a valid message id from **THIS** channel!');
+    }
   }
 
   await msgToReact.react(client.emoji.thumbsUp);
   await msgToReact.react(client.emoji.thumbsDown);
   await msgToReact.react(client.emoji.neutral);
-  message.delete({ timeout: 1000 });
+  return message.delete({ timeout: 1000 });
 };
 
 module.exports.conf = {
-  guildOnly: false,
+  guildOnly: true,
   aliases: [],
   permLevel: 'User',
   args: 1,
