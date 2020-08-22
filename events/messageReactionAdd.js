db9e3f7dbdea56fda034ba6eb7d9af5b666b563b
Phoenix1128 <pdinsdale1128@gmail.com>
2020-08-22 10:25:42 -0400

    React to sign up

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,6 +1,44 @@
+/* eslint-disable consistent-return */
 module.exports = async (client, messageReaction, user) => {
   if (user.bot || messageReaction.message.guild.id !== client.config.mainGuild) {
     return;
+  }
+
+  const reactionData = client.reactionSignUp.get('data');
+
+  if (reactionData.messageID === messageReaction.message.id) {
+    if (reactionData.reactions.includes(messageReaction.emoji.name)) {
+      const index = reactionData.reactions.findIndex((s) => s === messageReaction.emoji.name);
+      const namesToEdit = reactionData.names[index];
+      const newNames = `${namesToEdit === '' ? '' : `${namesToEdit.trim()},`} ${messageReaction.message.guild.members.cache.get(user.id).displayName}`;
+
+      let newSignUp = '';
+      for (let i = 0; i < reactionData.signUpSheet.length; i++) {
+        if (i === index) {
+          newSignUp += `${reactionData.signUpSheet[i]}${newNames}\n`;
+        } else {
+          newSignUp += `${reactionData.signUpSheet[i]}${reactionData.names[i]}\n`;
+        }
+      }
+
+      const { names } = reactionData;
+      names.splice(index, 1, newNames);
+
+      client.reactionSignUp.set('data', names, 'names');
+      const mod = client.reactionSignUp.ensure(user.id, {
+        signUpsThisWeek: 0, hoursThisWeek: 0, channels: [], start: null,
+      });
+
+      if (mod.channels.length === 0) {
+        client.reactionSignUp.inc(user.id, 'signUpsThisWeek');
+        client.reactionSignUp.set(user.id, Date.now(), 'start');
+      }
+
+      client.reactionSignUp.push(user.id, messageReaction.emoji.name, 'channels');
+
+      const msg = client.channels.cache.get(reactionData.channelID).messages.cache.get(reactionData.messageID);
+      return msg.edit(newSignUp);
+    }
   }
 
   const reactionRoleMenu = client.reactionRoleDB.get(messageReaction.message.id);
--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -1,6 +1,46 @@
+const moment = require('moment');
+
+/* eslint-disable consistent-return */
 module.exports = async (client, messageReaction, user) => {
   if (user.bot || messageReaction.message.guild.id !== client.config.mainGuild) {
     return;
+  }
+
+  const reactionData = client.reactionSignUp.get('data');
+
+  if (reactionData.messageID === messageReaction.message.id) {
+    if (reactionData.reactions.includes(messageReaction.emoji.name)) {
+      const index = reactionData.reactions.findIndex((s) => s === messageReaction.emoji.name);
+      const namesToEdit = reactionData.names[index].split(',');
+      const nameToRemove = namesToEdit.findIndex((s) => s.trim() === messageReaction.message.guild.members.cache.get(user.id).displayName);
+      namesToEdit.splice(nameToRemove, 1);
+
+      let newSignUp = '';
+      for (let i = 0; i < reactionData.signUpSheet.length; i++) {
+        if (i === index) {
+          newSignUp += `${reactionData.signUpSheet[i]}${namesToEdit.join(', ').trim()}\n`;
+        } else {
+          newSignUp += `${reactionData.signUpSheet[i]}${reactionData.names[i]}\n`;
+        }
+      }
+
+      const { names } = reactionData;
+      names.splice(index, 1, namesToEdit.join(', '));
+      client.reactionSignUp.set('data', names, 'names');
+
+      client.reactionSignUp.remove(user.id, messageReaction.emoji.name, 'channels');
+
+      if (client.reactionSignUp.get(user.id).channels.length === 0) {
+        const start = client.reactionSignUp.get(user.id, 'start');
+        const duration = moment.duration(moment().diff(moment(start)));
+        const hoursThisWeek = Math.round(duration.asHours() * 1000) / 1000;
+        client.reactionSignUp.math(user.id, '+', hoursThisWeek, 'hoursThisWeek');
+        client.reactionSignUp.set(user.id, null, 'start');
+      }
+
+      const msg = client.channels.cache.get(reactionData.channelID).messages.cache.get(reactionData.messageID);
+      return msg.edit(newSignUp);
+    }
   }
 
   const reactionRoleMenu = client.reactionRoleDB.get(messageReaction.message.id);
