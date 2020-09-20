fda7c9a5b8d3a6adc0f0717afa2863ae77912be2
Rootspring <66250972+cheesycod@users.noreply.github.com>
2020-09-20 17:13:37 +0530

    Bring proper purges

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -1,19 +1,32 @@
 // eslint-disable-next-line no-unused-vars
+
+// Simple sleep function (for discord ratelimits)
+function sleep(seconds) {
+  milliseconds = seconds*1000;
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
 module.exports.run = async (client, message, args, level) => {
   // Gets the delete count
   const deleteCount = parseInt(args[0], 10);
 
   await message.delete();
-  // If delete count is nonexistent, less than 1 or greater than 100, display this
-  if (!deleteCount || deleteCount < 1 || deleteCount > 100) {
-    return client.error(message.channel, 'Invalid Number of Messages to Purge!', 'Please provide a number **between 1 and 100** for the number of messages to delete!');
+  // If delete count is nonexistent or less than 1, display this
+  if (!deleteCount || deleteCount < 1) {
+    return client.error(message.channel, 'Invalid Number of Messages to Purge!', 'Please provide a number for the number of messages to delete!');
   }
 
   const descision = await client.reactPrompt(message, `Would you like to delete ${deleteCount} messages from <#${message.channel.id}>?`);
 
   if (descision) {
-    return message.channel.bulkDelete(deleteCount)
-      .catch((error) => client.error(message.channel, 'Purge Failed!', `Couldn't delete messages because: \`${error}\``));
+    for(var i = 0; i >= deleteCount; i++) {
+       message.channel.bulkDelete(deleteCount).catch((error) => client.error(message.channel, 'Purge Failed!', `Couldn't delete messages because: \`${error}\``));
+       sleep(0.2); // No more ratelimits pls.
+    };
   }
   return client.error(message.channel, 'Messages Not Purged!', 'The prompt timed out, or you selected no.');
 };
@@ -30,5 +43,5 @@ module.exports.help = {
   category: 'moderation',
   description: 'Purges the stated number of messages in a channel',
   usage: 'purge <number>',
-  details: "<number> => From 1-100. I wish it could be more but Discord's API doesn't allow that...",
+  details: "<number> => Any number of messages to purge",
 };
