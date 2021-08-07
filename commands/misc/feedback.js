bfa374c29fcfb5f716073cdc39da834c02cf9d23
Phoenix1128 <pdinsdale1128@gmail.com>
2021-08-06 20:43:22 -0400

    feedback opt in and out commands

--- commands/misc/feedback.js
+++ commands/misc/feedback.js
@@ -13,73 +13,100 @@ module.exports.run = async (client, message, args, level) => {
 
   if (!user) {
     if (level >= 6) {
-      const feedbackMessage = await message.channel.messages.fetch(args[1]);
-      if (!feedbackMessage || !feedbackMessage.embeds[0]) {
-        return client.error(message.channel, 'Invalid Message ID!', 'Please provide a valid message id of the feedback you wish to send!');
-      }
-
-      const originalEmbed = feedbackMessage.embeds[0];
-      const recipient = await client.users.fetch(originalEmbed.footer.text.split('-')[1].trim());
-
-      const sendFeedback = async (feedback) => {
-        const confirmPrompt = await message.channel.send(`Are you sure you want to send **${recipient.tag}** the following feedback?\n\`\`\`\n${feedback}\`\`\``);
-        await confirmPrompt.react(client.emoji.checkMark);
-        await confirmPrompt.react(client.emoji.redX);
-
-        const filter = (reaction, userReacted) => [client.emoji.checkMark, client.emoji.redX].includes(reaction.emoji.name)
-            && userReacted.id === message.author.id;
-
-        let decision = false;
-        await confirmPrompt.awaitReactions(filter, { max: 1, time: 1200000, errors: ['time'] })
-          .then((collected) => {
-            if (collected.first().emoji.name === client.emoji.checkMark) {
-              decision = true;
-            }
-          })
-          .catch(() => client.error(message.channel, 'Confirmation Prompt Timed Out!', 'Please rerun the command to attempt to send feedback again!'));
-
-        if (decision) {
-          // Try to send dm to recipient
-          try {
-            const dmChannelRecip = await recipient.createDM();
-            const newEmbed = new Discord.MessageEmbed()
-              .setColor('RANDOM')
-              .setTimestamp()
-              .setTitle("You've Been Sent Feedback!")
-              .setDescription(feedback);
-
-            await dmChannelRecip.send(newEmbed);
-            client.success(message.channel, 'Feedback Sent!', `I've successfully sent **${recipient.tag}** feedback!`);
-          } catch (e) {
-            return client.error(message.channel, 'Failed to Send DM!', "I've failed to send the requested feedback! The recipient may have DMs off!");
-          }
-
-          // try to send dm to original sender
-          try {
-            const originalSender = await client.users.fetch(originalEmbed.footer.text.split('-')[0].trim());
-            const dmChannelOriginal = await originalSender.createDM();
-
-            return client.success(dmChannelOriginal, 'Feedback Confirmed and Sent!', `Your feedback for **${recipient.tag}** was confirmed and sent successfully!`);
-          } catch (e) {
-            return console.error(e);
-          }
-        } else {
-          return client.error(message.channel, 'Not Sending Feedback!', `Feedback was not sent to **${recipient.tag}**!`);
+      if (args[0] === 'optout' || args[0] === 'oo') {
+        let userToOptOut;
+        try {
+          userToOptOut = await client.users.fetch(args[1]);
+        } catch (err) {
+          return client.error(message.channel, 'Invalid Member', "Please mention a valid member of the server to set as 'Opted Out' of feedback!");
         }
-      };
 
-      switch (args[0]) {
-        case 'send':
-        case 's':
-          await sendFeedback(originalEmbed.description);
-          break;
-        case 'edit':
-        case 'e':
-          await sendFeedback(args.slice(2).join(' '));
-          break;
-        default:
-          client.error(message.channel, 'Invalid Operator!', 'Please specify whether to edit (e) or send (s) the requested feedback!');
-          break;
+        client.userDB.set(userToOptOut.id, true, 'optOutFeedback');
+        client.success(message.channel, 'Successfully Opted Out!', `I've successfully set **${userToOptOut.tag}** as 'Opted Out' of feedback!`);
+      } else if (args[0] === 'optin' || args[0] === 'oi') {
+        let userToOptIn;
+        try {
+          userToOptIn = await client.users.fetch(args[1]);
+        } catch (err) {
+          return client.error(message.channel, 'Invalid Member', "Please mention a valid member of the server to set as 'Opted In' to feedback!");
+        }
+
+        client.userDB.set(userToOptIn.id, false, 'optOutFeedback');
+        client.success(message.channel, 'Successfully Opted In!', `I've successfully set **${userToOptIn.tag}** as 'Opted In' to feedback!`);
+      } else {
+        const feedbackMessage = await message.channel.messages.fetch(args[1]);
+        if (!feedbackMessage || !feedbackMessage.embeds[0]) {
+          return client.error(message.channel, 'Invalid Message ID!', 'Please provide a valid message id of the feedback you wish to send!');
+        }
+
+        const originalEmbed = feedbackMessage.embeds[0];
+        const recipient = await client.users.fetch(originalEmbed.footer.text.split('-')[1].trim());
+
+        const optedOut = client.userDB.ensure(recipient.id, client.config.userDBDefaults).optOutFeedback;
+        if (optedOut) {
+          return client.error(message.channel, 'Opted Out', `**${recipient.tag}** has opted out of receiving feedback!`);
+        }
+
+        const sendFeedback = async (feedback) => {
+          const confirmPrompt = await message.channel.send(`Are you sure you want to send **${recipient.tag}** the following feedback?\n\`\`\`\n${feedback}\`\`\``);
+          await confirmPrompt.react(client.emoji.checkMark);
+          await confirmPrompt.react(client.emoji.redX);
+
+          const filter = (reaction, userReacted) => [client.emoji.checkMark, client.emoji.redX].includes(reaction.emoji.name)
+                  && userReacted.id === message.author.id;
+
+          let decision = false;
+          await confirmPrompt.awaitReactions(filter, { max: 1, time: 1200000, errors: ['time'] })
+            .then((collected) => {
+              if (collected.first().emoji.name === client.emoji.checkMark) {
+                decision = true;
+              }
+            })
+            .catch(() => client.error(message.channel, 'Confirmation Prompt Timed Out!', 'Please rerun the command to attempt to send feedback again!'));
+
+          if (decision) {
+            // Try to send dm to recipient
+            try {
+              const dmChannelRecip = await recipient.createDM();
+              const newEmbed = new Discord.MessageEmbed()
+                .setColor('RANDOM')
+                .setTimestamp()
+                .setTitle("You've Been Sent Feedback!")
+                .setDescription(feedback);
+
+              await dmChannelRecip.send(newEmbed);
+              client.success(message.channel, 'Feedback Sent!', `I've successfully sent **${recipient.tag}** feedback!`);
+            } catch (e) {
+              return client.error(message.channel, 'Failed to Send DM!', "I've failed to send the requested feedback! The recipient may have DMs off!");
+            }
+
+            // try to send dm to original sender
+            try {
+              const originalSender = await client.users.fetch(originalEmbed.footer.text.split('-')[0].trim());
+              const dmChannelOriginal = await originalSender.createDM();
+
+              return client.success(dmChannelOriginal, 'Feedback Confirmed and Sent!', `Your feedback for **${recipient.tag}** was confirmed and sent successfully!`);
+            } catch (e) {
+              return console.error(e);
+            }
+          } else {
+            return client.error(message.channel, 'Not Sending Feedback!', `Feedback was not sent to **${recipient.tag}**!`);
+          }
+        };
+
+        switch (args[0]) {
+          case 'send':
+          case 's':
+            await sendFeedback(originalEmbed.description);
+            break;
+          case 'edit':
+          case 'e':
+            await sendFeedback(args.slice(2).join(' '));
+            break;
+          default:
+            client.error(message.channel, 'Invalid Operator!', 'Please specify whether to edit (e) or send (s) the requested feedback!');
+            break;
+        }
       }
     } else {
       return client.error(message.channel, 'Invalid Member', 'Please mention a valid member of the server!');
@@ -96,13 +123,14 @@ module.exports.run = async (client, message, args, level) => {
 
     const feedbackMsg = args.slice(1).join(' ');
     const feedbackCh = client.channels.cache.get('871526192707141653');
+    const optedOut = client.userDB.ensure(user.id, client.config.userDBDefaults).optOutFeedback;
     const { displayColor } = await client.guilds.cache.get(client.config.mainGuild).members.fetch(user.id);
 
     const feedbackEmbed = new Discord.MessageEmbed()
-      .setColor(displayColor)
+      .setColor(optedOut ? undefined : displayColor)
       .setTimestamp()
       .setAuthor(message.author.tag, message.author.avatarURL({ dynamic: true }))
-      .setTitle(`From ${message.author.tag} to ${user.tag}`)
+      .setTitle(`From ${message.author.tag} to ${user.tag}${optedOut ? ' [OPTED OUT]' : ''}`)
       .setDescription(feedbackMsg)
       .setFooter(`${message.author.id} - ${user.id}`);
 
